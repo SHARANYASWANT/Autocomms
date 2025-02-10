de6f1d441d0f3fa3fe689e6b063729bf45cb48be
SHARANYASWANT <sharanyaswant.06@gmail.com>
2025-02-10 14:57:46 +0530

    Added Middleware files
    	modified:   .env
    	modified:   .gitignore
    	modified:   controllers/authController.js
    	modified:   middleware/authenticateUser.js

--- middleware/authenticateUser.js
+++ middleware/authenticateUser.js
@@ -1,25 +1,57 @@
-// middleware/authenticateTeacher.js
-import { verify } from "jsonwebtoken";
+import pool from '../config/db.js';
 
-const authenticateTeacher = (req, res, next) => {
-  const authHeader = req.header("Authorization");
-  if (!authHeader) {
-    return res.status(401).json({ error: "No token provided, authorization denied" });
-  }
-  
-  const token = authHeader.split(" ")[1]; // Expected format: "Bearer <token>"
-  if (!token) {
-    return res.status(401).json({ error: "Token missing" });
-  }
-  
+export async function validateSessionMiddleware(req, res, next) {
   try {
-    const decoded = verify(token, process.env.JWT_SECRET);
-    // Attach the teacher's ID from the token to the request object
-    req.user = { teacher_id: decoded.teacher_id };
+    const sessionID = req.cookies.sessionID;
+
+    if (!sessionID) {
+      return res.status(401).json({ error: 'Invalid session ID' });
+    }
+
+    const result = await pool.query('SELECT * FROM sessions WHERE session_id = $1', [sessionID]);
+
+    if (result.rows.length === 0) {
+      return res.status(401).json({ error: 'Invalid session ID' });
+    }
+
+    const session = result.rows[0];
+    if (new Date() > new Date(session.expires_at)) {
+      // await pool.query('DELETE FROM sessions WHERE session_id = $1', [sessionID]);
+      return res.status(401).json({ error: 'Invalid session ID' });
+    }
+
+    if (session.session_id !== sessionID) {
+      return res.status(401).json({ error: 'Invalid session ID' });
+    }
+    
     next();
   } catch (error) {
-    res.status(401).json({ error: "Token is not valid" });
+    res.status(500).json({ error: 'Session validation failed' });
   }
-};
+}
 
-export default authenticateTeacher;
+export async function validateCsrfMiddleware(req, res, next) {
+  try {
+    const csrfToken = req.cookies.csrfToken;
+
+    if (!csrfToken) {
+      return res.status(401).json({ error: 'Invalid CSRF token' });
+    }
+
+    const result = await pool.query('SELECT * FROM sessions WHERE csrf_token = $1', [csrfToken]);
+
+    if (result.rows.length === 0) {
+      return res.status(401).json({ error: 'Invalid CSRF token' });
+    }
+
+    const csrf = result.rows[0];
+    if (csrf.csrf_token !== csrfToken) {
+      return res.status(401).json({ error:'Invalid CSRF token' });
+    }
+
+    next();
+  } catch (error) {
+    res.status(500).json({ error: 'CSRF token validation failed' });
+  }
+}
+
