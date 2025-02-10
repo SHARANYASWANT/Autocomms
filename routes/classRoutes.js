f6b65283e3f7ab92a44ceed97eed1e659acf0ed9
MohamedHamdanA <mdhamdan1111@gmail.com>
2025-02-10 16:09:53 +0530

    implemented middleware

--- routes/classRoutes.js
+++ routes/classRoutes.js
@@ -6,20 +6,20 @@ const storage = multer.memoryStorage(); // Store files in memory as buffers
 const upload = multer({ storage });
 
 import { getClassesForUser, createClass, addGoogleForm, getClassWithForms } from "../controllers/classController.js";
-// import authenticateUser from "../middleware/authenticateUser.js";
+import {validateCsrfMiddleware, validateSessionMiddleware} from "../middleware/authenticateUser.js";
 
 // Get all classes for the authenticated User
-router.get("/my-classes",  getClassesForUser);
+router.get("/my-classes",validateCsrfMiddleware, validateSessionMiddleware,  getClassesForUser);
 
 // Ensure that upload.single('file') is used so that:
 // - The file field with key "file" is processed and available as req.file
 // - All other form-data fields are parsed and available in req.body
-router.post("/", upload.single("file"), createClass);
+router.post("/",validateCsrfMiddleware, validateSessionMiddleware, upload.single("file"), createClass);
 
 // Add a Google Form to a specific class
-router.post("/:classId/forms", addGoogleForm);
+router.post("/:classId/forms",validateCsrfMiddleware, validateSessionMiddleware, addGoogleForm);
 
 // Get a class (with its associated Google Forms)
-router.get("/:classId", getClassWithForms);
+router.get("/:classId",validateCsrfMiddleware, validateSessionMiddleware, getClassWithForms);
 
 export default router;
